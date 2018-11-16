544a7197d134040b32cd11dbbb379b9468a29644
Apple <apple@AppledeMac-mini.local>
2018-11-16 11:33:36 +0800

    增加app 状态描述

--- src/main/java/com/lkl/ansuote/hdqlibrary/base/ConstantValues.java
+++ src/main/java/com/lkl/ansuote/hdqlibrary/base/ConstantValues.java
@@ -1,11 +1,29 @@
 package com.lkl.ansuote.hdqlibrary.base;
 
 public class ConstantValues {
+    /**
+     * 应用被杀
+     */
     public static final int STATUS_FORCE_KILLED = -1;
-    public static final int STATUS_LOGOUT = 0;
+    /**
+     * 注销,退出系统
+     */
+    //public static final int STATUS_LOGOUT = 0;
+    /**
+     * 未登录
+     */
     public static final int STATUS_OFFLINE = 1;
+    /**
+     * 登录状态
+     */
     public static final int STATUS_ONLINE = 2;
+    /**
+     * 用户被踢或者TOKEN失效
+     */
     public static final int STATUS_KICK_OUT = 3;
+    /**
+     * 全部界面退出
+     */
     public static final int STATUS_FINISH_ALL = 4;
 
     public static final String KEY_HOME_ACTION = "key_home_action";
